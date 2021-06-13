296eae8456753a59dc02b6f945fd62638348671d
Kirill Sakun <kirillsakun03@gmail.com>
2021-06-13 20:20:25 +0300

    init

---
+++ js/test.js
@@ -0,0 +1,16 @@
+(function someAwesomeFunction() {
+  const a = 'test';
+  var b = "some info";
+  let c = {
+    'd': 123123,
+    a: a
+  }
+
+  console.log(c)
+  return a;
+}());
+
+
+const texts = dacument.querySelectorAll('p');
+
+console.log(texts.lenght);
